d79cda2359a897373574b140f5dd68300ccd6d20
Paul Smith <mail@airfront.co.uk>
2016-10-29 20:59:44 +0100

    adds theme overrides dir for fractal

--- fractal.js
+++ fractal.js
@@ -3,10 +3,13 @@
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
 
-// include nunjucks filters from external file
+/* require the mandelbrot theme module */
+const mandelbrot = require('@frctl/mandelbrot');
+
+/* include nunjucks filters from external file */
 const nunjfilters = require(__dirname + '/lib/filters')();
 
-/* Configure nunjucks adapter for fractal merging in filters from nunjfilters */
+/* configure nunjucks adapter for fractal merging in filters from nunjfilters */
 const nunj = require('@frctl/nunjucks')(Object.assign({
     globals: {
         // global-name: global-val
@@ -16,23 +19,41 @@ const nunj = require('@frctl/nunjucks')(Object.assign({
     }
 }, nunjfilters));
 
+/* create a new instance with custom config options */
+const themeconfig = mandelbrot({
+    skin: "black",
+    nav: ["docs", "components"]
+});
+
+/* fractal instantiation
+----------------------------------------------------------------------------- */
+
+/* Set the title of the project */
+fractal.set('project.title', 'DWP Frontend Toolkit');
+
 // register the Nunjucks adapter for components
 fractal.components.engine(nunj);
 
-// register the Nunjucks adapter for documentation
-fractal.docs.engine(nunj);
-
-// look for files with a .nunj file extension
+// component look for files with a .nunj file extension
 fractal.components.set('ext', '.nunj');
-
-// look for files with a .nunj file extension
-fractal.docs.set('ext', '.nunj');
-
-/* Set the title of the project */
-fractal.set('project.title', 'DWP Front-end toolkit');
 
 /* Tell Fractal where the components will live */
 fractal.components.set('path', __dirname + '/src/components');
 
+// register the Nunjucks adapter for documentation
+fractal.docs.engine(nunj);
+
+// docs to look for files with a .nunj file extension
+fractal.docs.set('ext', '.nunj');
+
 /* Tell Fractal where the documentation pages will live */
 fractal.docs.set('path', __dirname + '/src/docs');
+
+// specify a directory to hold the theme override templates
+themeconfig.addLoadPath(__dirname + '/theme_overrides');
+
+/* tell fractal to use the configured theme by default */
+fractal.web.theme(themeconfig);
+
+/* Tell fractal to use browsersync by default */
+fractal.web.set('server.sync', true);
